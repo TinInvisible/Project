6b496bfb3881bef5031f123e0de64fda70b625a7
TinInvisible <botomtinlon@gmail.com>
2022-12-07 16:12:29 +0700

    fix

--- mvc-customer/components/products/Service.js
+++ mvc-customer/components/products/Service.js
@@ -12,5 +12,17 @@ exports.filter = (name) => {
 exports.filter1 = (name) => {
   return productRepository.filter1(name);
 }
+exports.filter2 = (name) => {
+  return productRepository.filter2(name);
+}
+exports.filter3 = (name) => {
+  return productRepository.filter3(name);
+}
+exports.filter4 = (name) => {
+  return productRepository.filter4(name);
+}
+exports.filter5 = (name) => {
+  return productRepository.filter5(name);
+}
 
 exports.get = (id) => productRepository.get(id);
--- mvc-customer/components/products/mainPageController.js
+++ mvc-customer/components/products/mainPageController.js
@@ -5,12 +5,28 @@ const qs = require('qs');
 exports.list = async (req, res) => {
   const { name: nameFilter } = req.query;
   const { category } = req.query;
+  const { branding } = req.query;
+  const { price1 } = req.query;
+  const { price2 } = req.query;
+  const { price3 } = req.query;
   let products = [];
   if (nameFilter) {
     products = await productService.filter(nameFilter);
   }
   else if (category){
     products = await productService.filter1(category);
+  }
+  else if (branding){
+    products = await productService.filter2(category);
+  }
+  else if (price1){
+    products = await productService.filter3(category);
+  }
+  else if (price2){
+    products = await productService.filter4(category);
+  }
+   else if (price3){
+    products = await productService.filter5(category);
   }
   else
     products = await productService.getAll();
