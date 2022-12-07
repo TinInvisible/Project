77266c3ff2b66700bd262263f4a929ca44908949
TinInvisible <botomtinlon@gmail.com>
2022-12-07 20:53:35 +0700

    fix-bug

--- mvc-customer/components/products/productsRepository.js
+++ mvc-customer/components/products/productsRepository.js
@@ -2,32 +2,32 @@
 const db = require('../../db');
 
 exports.getAll = async () => {
-  const result =  await db.connection.execute('select * from customer_products.productdetail');
+  const result =  await db.connection.execute('select * from productdetail');
   return result[0];
 }
 
 exports.filter = async (name) => {
-  const result =  await db.connection.execute("SELECT * FROM customer_products.productdetail where Name like ?", [`%${name}%`]);
+  const result =  await db.connection.execute("SELECT * FROM productdetail where Name like ?", [`%${name}%`]);
   return result[0];
 }
 exports.filter1 = async (name) => {
-  const result =  await db.connection.execute("SELECT * FROM customer_products.productdetail where Category = ?", [name]);
+  const result =  await db.connection.execute("SELECT * FROM productdetail where Category = ?", [name]);
   return result[0];
 }
 exports.filter2 = async (name) => {
-  const result =  await db.connection.execute("SELECT * FROM customer_products.productdetail where Branding = ?", [name]);
+  const result =  await db.connection.execute("SELECT * FROM productdetail where Branding = ?", [name]);
   return result[0];
 }
 exports.filter3 = async () => {
-  const result =  await db.connection.execute("SELECT * FROM customer_products.productdetail where Price <= 50");
+  const result =  await db.connection.execute("SELECT * FROM productdetail where Price <= 50");
   return result[0];
 }
 exports.filter4 = async () => {
-  const result =  await db.connection.execute("SELECT * FROM customer_products.productdetail where Price > 50 and Price < 100");
+  const result =  await db.connection.execute("SELECT * FROM productdetail where Price > 50 and Price < 100");
   return result[0];
 }
 exports.filter5 = async () => {
-  const result =  await db.connection.execute("SELECT * FROM customer_products.productdetail where Price > 100");
+  const result =  await db.connection.execute("SELECT * FROM productdetail where Price > 100");
   return result[0];
 }
 exports.sort_price_asc = async () => {
