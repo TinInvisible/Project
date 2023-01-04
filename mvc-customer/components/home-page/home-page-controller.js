4b84c36c89526d507cc9a489eadeab1f07598825
TinInvisible <botomtinlon@gmail.com>
2023-01-04 12:52:44 +0700

    update

--- mvc-customer/components/home-page/home-page-controller.js
+++ mvc-customer/components/home-page/home-page-controller.js
@@ -14,6 +14,7 @@ exports.getHomePage = (req, res) => {
 exports.getCheckOut = (req, res) => {
     if (!req.session.cart) {
         res.redirect('/home-page/shop/shopping-cart');
+        return;
     }
     res.render('products/checkout');
 }
@@ -76,7 +77,7 @@ exports.editProfile = async (req, res, next) => {
                 res.render('products/profile', { error4: 'Enter new password' });
                 return;
             }
-            if(new_pass.length < 6){
+            if (new_pass.length < 6) {
                 res.render('products/profile', { error4: 'Your password must contain at least 6 character' });
                 return;
             }
@@ -88,22 +89,28 @@ exports.editProfile = async (req, res, next) => {
 }
 
 exports.insertShippingDetail = async (req, res) => {
-    const { firstName, lastName, Country, Address, townCity, postCode, Phone, Email, Day, Month, Year } = req.body;
-    let list_products = await cart_service.cartDetails(req.session.cart);
-    if (list_products.products && firstName && lastName && Country && Address && townCity && postCode && Phone, Email && Day && Month && Year) {
+    const { firstName, lastName, Country, Address, townCity, postCode, Phone, Email, Day, Month, Year, checkout } = req.body;
+    if (checkout) {
+        let list_products = await cart_service.cartDetails(req.session.cart);
+        if (list_products.products && firstName && lastName && Country && Address && townCity && postCode && Phone, Email && Day && Month && Year) {
+            let Price = 0;
+            for (let i = 0; i < list_products.products.length; i++) {
+                Price += list_products.products[i].price;
+                let product = await product_service.get(parseInt(list_products.products[i].id));
+                console.log(list_products.products[i].id);
+                console.log(typeof (list_products.products[i].id));
+                let num_purchase = list_products.products[i].quantity + product.Total_purchase;
 
-        let Price = 0;
-        for (let i = 0; i < list_products.products.length; i++) {
-            Price += list_products.products[i].price;
-            let product = await product_service.get(parseInt(list_products.products[i].id));
-            console.log(list_products.products[i].id);
-            console.log(typeof (list_products.products[i].id));
-            let num_purchase = list_products.products[i].quantity + product.Total_purchase;
-
-            await service.update_total_purchase(num_purchase, list_products.products[i].id)
+                await service.update_total_purchase(num_purchase, list_products.products[i].id)
+            }
+            let day = Year + "-" + Month + "-" + Day;
+            await service.insertShippingDetail(firstName, lastName, Country, Address, townCity, postCode, Phone, Email, Price, day);
+            res.render('products/checkout',{notice: 'Complete'});
+            return;
         }
-        let day = Year + "-" + Month + "-" + Day;
-        await service.insertShippingDetail(firstName, lastName, Country, Address, townCity, postCode, Phone, Email, Price, day);
+        else{
+            res.render('products/checkout',{notice:'Please input full of the information'});
+            return;
+        }
     }
-    res.redirect('/home-page/checkout');
 }
